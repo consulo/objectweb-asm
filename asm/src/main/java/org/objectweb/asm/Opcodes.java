6e2da524a9c7f37c273f562d32a3f13c3ff5e99b
VISTALL <vistall.valeriy@gmail.com>
2021-02-06 21:19:54 +0300

    asm9

--- asm/src/main/java/org/objectweb/asm/Opcodes.java
+++ asm/src/main/java/org/objectweb/asm/Opcodes.java
@@ -58,7 +58,7 @@ public interface Opcodes {
    */
   @Deprecated int ASM10_EXPERIMENTAL = 1 << 24 | 10 << 16 | 0 << 8;
 
-  int API_VERSION = ASM7; //[JB: convenience constant]
+  int API_VERSION = ASM9; //[JB: convenience constant]
 
   /*
    * Internal flags used to redirect calls to deprecated methods. For instance, if a visitOldStuff
